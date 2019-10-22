20f9694fb3540c8f8dd2f15d5f3c4badc47eda16
Nico Audy <nicoaudy1994@gmail.com>
2019-10-22 15:04:36 +0700

    deploy to nelify

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,7 +23,7 @@ const users = [
 const siteConfig = {
   title: "Cidro UI", // Title for your website.
   tagline: "React Native UI Library",
-  url: "https://your-docusaurus-test-site.com", // Your website URL
+  url: "https://serene-goldstine-9f6be6.netlify.com/", // Your website URL
   baseUrl: "/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
